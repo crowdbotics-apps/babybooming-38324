598ffe397e4d21f5cd4b6e9889e3ec2c7824bc30
Crowdbotics <team@crowdbotics.com>
2023-01-17 02:41:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "babybooming-38324",
+  url: "https://babybooming-38324.botics.co",
+  api: "https://babybooming-38324.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
